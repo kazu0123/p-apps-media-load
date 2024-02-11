a743ccadef32d6846cbfbb4db642116fd69f7c00
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-02-11 20:23:01 +0900

    add code

---
+++ script.js
@@ -0,0 +1,21 @@
+const number_max_input = document.getElementById("number_max_input");
+const reading_mode_input = document.getElementById("reading_mode_input");
+const reading_speed_input = document.getElementById("reading_speed_input");
+const output_area = document.getElementById("output_area");
+
+function genText() {
+    const readingMode = reading_mode_input.value;
+    const readingSpeed = reading_speed_input.value;
+
+    const numberMax = parseInt(number_max_input.value, 10);
+
+    let outputText = "";
+
+    Array.from({length: numberMax}).forEach((_, index) => {
+        const fileNumber = index + 1;
+        const fileName = `A_${readingMode}_${fileNumber}_${readingSpeed}`;
+        outputText += fileName + ";";
+    });
+
+    output_area.value = outputText;
+}
