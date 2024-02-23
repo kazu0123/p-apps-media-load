76cf76ec0d9bfc825e60a428da8fc743bde2c2f9
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-02-23 17:34:21 +0900

    add page id input

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const number_max_input = document.getElementById("number_max_input");
 const reading_mode_input = document.getElementById("reading_mode_input");
 const reading_speed_input = document.getElementById("reading_speed_input");
+const page_id_input = document.getElementById("page_id_input");
 const output_area = document.getElementById("output_area");
 
 function genText() {
     const readingMode = reading_mode_input.value;
     const readingSpeed = reading_speed_input.value;
+    const pageId = page_id_input.value;
 
     const numberMax = parseInt(number_max_input.value, 10);
 
@@ -13,7 +15,7 @@ function genText() {
 
     Array.from({length: numberMax}).forEach((_, index) => {
         const fileNumber = index + 1;
-        const fileName = `A_${readingMode}_${fileNumber}_${readingSpeed}`;
+        const fileName = `${pageId}_${readingMode}_${fileNumber}_${readingSpeed}`;
         outputText += fileName + ";";
     });
 
